6d99b5668d13eceaf900d463e8a5c84f4a998f54
Chavindu Nuwanpriya <chavindun@gmail.com>
2025-11-22 22:53:06 +0530

    CRON_SECRET

--- api/serverless.js
+++ api/serverless.js
@@ -1,5 +1,14 @@
 export default async function handler(req, res) {
-    console.log("Triggered daily cron...");
-    await import("../fetch-rates.js");
+
+  // Authentication check for cron
+  if (
+    process.env.CRON_SECRET &&
+    req.headers["authorization"] !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).send("Unauthorized");
+  }
+
+    console.log("Cron triggered...");
+    await import("../public/fetch-rates.js");
     res.status(200).json({ success: true });
 }
